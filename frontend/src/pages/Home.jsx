50c1f1bcce8bcb145978b16a4c96bd88290d1de9
keroloussamy <keroloussamy98@gmail.com>
2023-02-08 19:50:53 +0200

    Add startup frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// Home page component
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to my Home Page!</h1>
+      <p>This is the home page of my website.</p>
+    </div>
+  );
+};
+
+export default Home;
