ccb474fe6db80658434f2cd8ba51a59ff3e70908
keroloussamy <keroloussamy98@gmail.com>
2023-02-13 14:08:48 +0200

    Add jwt and login method

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,7 +1,22 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
+
+// login a user
 const loginUser = async (req, res) => {
-  res.json({ message : 'login user'})
+  const {email, password} = req.body
+
+  try {
+    const user = await User.login(email, password)
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
 const signupUser = async (req, res) => {
@@ -9,8 +24,9 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password)
+    const token = createToken(user._id)
 
-    res.status(200).json({email, user})
+    res.status(200).json({email, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
--- backend/models/user.js
+++ backend/models/user.js
@@ -19,11 +19,12 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function(email, password) { // should be regular function to use this keyword.
-  if (!validator.isEmail(email)) throw Error('Invalid email');
-  if (!validator.isStrongPassword(password)) throw Error('Password is not strong enough');
+  if (!email || !password) throw new Error('All fields must be filled')
+  if (!validator.isEmail(email)) throw new Error('Invalid email');
+  if (!validator.isStrongPassword(password)) throw new Error('Password is not strong enough');
   
   const userExists = await this.findOne({ email });
-  if (userExists) throw Error('Email already in use');
+  if (userExists) throw new Error('Email already in use');
 
   const salt = await bcrypt.genSalt(10); // salt is a random string that is added to the password before hashing it. Optional.
   const hashedPassword = await bcrypt.hash(password, salt);
@@ -31,6 +32,19 @@ userSchema.statics.signup = async function(email, password) { // should be regul
   const newUser = await this.create({ email, password: hashedPassword });
 
   return newUser;
+}
+
+// static login method
+userSchema.statics.login = async function(email, password) {
+  if (!email || !password) throw new Error('All fields must be filled')
+
+  const user = await this.findOne({ email })
+  if (!user) throw new Error('Incorrect email')
+
+  const isMatch = await bcrypt.compare(password, user.password)
+  if (!isMatch) throw new Error('Incorrect password')
+
+  return user
 }
 
 module.exports = mongoose.model('User', userSchema)
