cc520841840f7ac515335e9a39c23fd0bf8956b5
keroloussamy <keroloussamy98@gmail.com>
2023-02-19 14:36:13 +0200

    Add 404 handler

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,31 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutsRouter);
 app.use('/api/users', userRoutes)
 
+// General error handling
+// app.use((err, req, res, next) => {
+//   res.status(500).json({ err });
+// });
+
+// 404
+app.use(function(req, res, next) {
+  res.status(404);
+
+  // respond with html page
+  // if (req.accepts('html')) {
+  //   res.render('404', { url: req.url });
+  //   return;
+  // }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  // res.type('txt').send('Not found');
+});
+
 // connect to db
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URL)
