7db6e611f17231239cad26d6a0c024eeb1d89538
keroloussamy <keroloussamy98@gmail.com>
2023-02-24 10:56:34 +0200

    Fix typo errors

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import useAuthContext from '../context/AuthContext'
+import useAuthContext from './useAuthContext'
 
 const useLogin = () => {
   const [error, setError] = useState(null)
